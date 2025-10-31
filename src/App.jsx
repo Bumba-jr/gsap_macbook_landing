251fd0f45740f3813b1bf68e998439ad4eab54cb
Bumba <Iliyashatami20@gmail.com>
2025-10-31 08:06:42 +0100

    Implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import NavBar from "./components/NavBar.jsx";
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
     return (
         <main>
             <NavBar />
-            <h1 className={"text-3xl font-bold underline text-indigo-400 bg-blue-500"}>Hello, Apple Macbook pro!</h1>
+            <Hero />
         </main>
     )
 }
