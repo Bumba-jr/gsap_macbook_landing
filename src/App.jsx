8cb5a058585d889994259329d797923f38d159a9
Bumba <Iliyashatami20@gmail.com>
2025-10-31 07:38:26 +0100

    initial  commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from "./components/NavBar.jsx";
+
+const App = () => {
+    return (
+        <main>
+            <NavBar />
+            <h1 className={"text-3xl font-bold underline text-indigo-400 bg-blue-500"}>Hello, Apple Macbook pro!</h1>
+        </main>
+    )
+}
+export default App
